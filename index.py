0583c3407530a4a4215cff190e5abb2a03256fe0
Daniel Golia Conti <contidan@msu.edu>
2025-04-01 16:41:12 -0400

    Found that day 4 was plotting calories up to 200, while the other days recorded them up to 400, and proved it using data science techniques

--- index.py
+++ index.py
@@ -1422,3 +1422,74 @@ def plot_the_dataset(feature, label, number_of_points_to_plot):
 
     # Create a scatter plot from n random points of the dataset.
     random_examples = training_df.sample(n=number_of_points_to_plot)
+    plt.scatter(random_examples[feature], random_examples[label])
+
+    # Render the scatter plot
+    print(plt.show())
+
+def plot_a_contiguous_portion_of_dataset(feature, label, start, end):
+    """Plot the data points from start to end."""
+
+    # Label the axes.
+    plt.xlabel(feature + "Dat")
+    plt.ylabel(label)
+
+    # Create a scatter plot.
+    plt.scatter(training_df[feature][start:end], training_df[label][start:end])
+
+    # Render the scatter plot.
+    print(plt.show())
+
+print("Defined the following functions:")
+print(" * plot_the_dataset")
+print(" * plot_a_contiguous_portion_of_dataset")
+
+# plot_the_dataset("calories", "test_score", number_of_points_to_plot=1400)
+
+# Get statistics on Week 0
+print(training_df[0:350].describe())
+
+# Get statistics on Week 1
+print(training_df[350:700].describe())
+
+# Get statistics on Week 2
+print(training_df[700:1050].describe())
+
+# Get statistics on Week 3
+print(training_df[1050:1400].describe())
+
+# for i in range(0,7):
+#     start = i * 50
+#     end = start + 49
+#     print("\nDay %d" % i)
+#     plot_a_contiguous_portion_of_dataset("calories", "test_score", start, end)
+
+print("Starting proof that thursday is being categorized differently")
+mean = 0
+count = 0
+for i in range(0,7):
+    count = 0
+    start = i * 50
+    end = start + 49
+    for j in range(start, end):
+        count += training_df.iloc[j]["calories"]
+    print(f"Mean calorie count for day {i}: {count/50}")
+
+running_total_of_thursday_calories = 0
+running_total_of_non_thursday_calories = 0
+count = 0
+for week in range(0,4):
+  for day in range(0,7):
+    for subject in range(0,50):
+      position = (week * 350) + (day * 50) + subject
+      if (day == 4):  # Thursday
+        running_total_of_thursday_calories += training_df['calories'][position]
+      else:  # Any day except Thursday
+        count += 1
+        running_total_of_non_thursday_calories += training_df['calories'][position]
+
+mean_of_thursday_calories = running_total_of_thursday_calories / 200
+mean_of_non_thursday_calories = running_total_of_non_thursday_calories / 1200
+
+print("The mean of Thursday calories is %.0f" % (mean_of_thursday_calories))
+print("The mean of calories on days other than Thursday is %.0f" % (mean_of_non_thursday_calories))
